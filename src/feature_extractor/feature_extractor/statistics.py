0434f045b0a77e37387263c4f920665fed36c145
Arion Zimmermann <arionz@caltech.edu>
2024-03-29 11:08:15 -0700

    Improve statistics on trajectories

--- src/feature_extractor/feature_extractor/statistics.py
+++ src/feature_extractor/feature_extractor/statistics.py
@@ -1,5 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
+import os
 
 class ErrorStatistics:
     def __init__(self):
@@ -9,17 +11,28 @@ class ErrorStatistics:
     
     def add_trajectory(self, keypoints, errors):
         if len(errors) > 0:
-            self._lengths.append(len(keypoints))
-            self._stddevs.append(int(100*np.std(errors))/100.0)
+
+            # plt.figure()
+            # plt.plot(keypoints)
+            # plt.plot(keypoints + errors)
+            # plt.show()
+
+            self._lengths.append(len(errors))
+            self._stddevs.append(int(100*np.linalg.norm(errors, ord=2)/np.sqrt(len(errors)))/100.0)
             
             self._count += 1
-            
-            self.display_statistics()
-        
+                    
     def display_statistics(self):
-        
-        print("Valid trajectories: " + str(self._count))
-        
-        if self._count % 10000 == 0:
-            plt.hist(self._stddevs, bins=np.linspace(0, np.mean(self._stddevs) * 3, int(np.sqrt(self._count))))
-            plt.show()
+        print("Average length: " + str(np.mean(self._lengths)))
+        print(np.median(self._stddevs))
+
+        i = 0
+        while os.path.exists("data%s.csv" % i):
+            i += 1
+
+        data = np.vstack((self._lengths, self._stddevs))
+        df = pd.DataFrame(data.T)
+        df.to_csv("data%s.csv" % i, index=False)
+
+        plt.hist(self._stddevs, bins=np.linspace(0, 2*np.median(self._stddevs), int(np.sqrt(self._count))))
+        plt.show()
