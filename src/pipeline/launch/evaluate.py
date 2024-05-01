9b6d329f93bc5067cdde08c0dd128b7ca9ac06c1
Arion Zimmermann <arionz@caltech.edu>
2024-04-30 19:05:59 -0700

    Add option to skip the synthesis when benchmarking a backend

--- src/pipeline/launch/evaluate.py
+++ src/pipeline/launch/evaluate.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import RegisterEventHandler, DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import UnlessCondition
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.event_handlers import OnExecutionComplete
@@ -11,7 +12,8 @@ def generate_launch_description():
         "size": LaunchConfiguration("size"),
         "mode": "test",
         "backend.type": LaunchConfiguration("backend"),
-        "eval.path": LaunchConfiguration("output")
+        "eval.path": LaunchConfiguration("output"),
+        "skip_synthesis": LaunchConfiguration("skip_synthesis")
     }
 
     dataset_params = os.path.join(get_package_share_directory('pipeline'), 'config', 'pipeline.yaml')
@@ -19,8 +21,9 @@ def generate_launch_description():
     create = OpaqueFunction(function=create_nodes, args=[run_params, dataset_params])
 
     return LaunchDescription([
+        DeclareLaunchArgument("skip_synthesis", default_value=["False"]),
         DeclareLaunchArgument("size"),
-        DeclareLaunchArgument("output"),
+        DeclareLaunchArgument("output", default_value=["/home/arion/AsteroidEvaluation"]),
         DeclareLaunchArgument("backend"),
         create
     ])
@@ -29,7 +32,8 @@ def create_nodes(context, run_params, dataset_params):
     eval_dir = context.perform_substitution(run_params["eval.path"])
     backend = context.perform_substitution(run_params["backend.type"])
     size = context.perform_substitution(run_params["size"])
-
+    skip_synthesis = context.perform_substitution(run_params["skip_synthesis"])
+    
     benchmarker_params = os.path.join(get_package_share_directory('pipeline'), 'config', backend + '.yaml')
 
     descriptor_io_override = {
@@ -91,18 +95,21 @@ def create_nodes(context, run_params, dataset_params):
         ]
     )
     
-    return [
-        RegisterEventHandler(
-            OnExecutionComplete(
-                target_action=synthesizer,
-                on_completion=[benchmarker]
-            )
-        ),
-        RegisterEventHandler(
-            OnExecutionComplete(
-                target_action=descriptor,
-                on_completion=[synthesizer]
-            )
-        ),
-        descriptor
-    ]
+    if skip_synthesis != "False":
+        return [benchmarker]
+    else:
+        return [
+            RegisterEventHandler(
+                OnExecutionComplete(
+                    target_action=synthesizer,
+                    on_completion=[benchmarker]
+                )
+            ),
+            RegisterEventHandler(
+                OnExecutionComplete(
+                    target_action=descriptor,
+                    on_completion=[synthesizer]
+                )
+            ),
+            descriptor
+        ]
