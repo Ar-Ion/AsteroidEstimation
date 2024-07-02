70e7db44b43d56a60a97e218a9fa33886bd6acc5
Arion Zimmermann <arionz@caltech.edu>
2024-07-01 21:58:37 -0700

    Fix loss functions by adding label smoothing and log-epsilon regularization

--- src/coffee_nn/coffee_nn/losses/cross_entropy.py
+++ src/coffee_nn/coffee_nn/losses/cross_entropy.py
@@ -3,19 +3,21 @@ from .loss import LossFunction
 
 class CrossEntropyLoss(LossFunction):
     def __init__(self):
-        pass
+        self._eps = 0.1
         
     def loss(self, true_dists, true_matches, pred_dists, pred_matches):       
-        # Ground truth
+        # Perhaps use label smoothing?
+        u = torch.rand_like(true_matches)*self._eps
+        true_matches_smoothed = (1-self._eps)*true_matches + self._eps*u/true_matches.shape[0]
         
-        true_bins = torch.maximum(torch.tensor(0), true_matches)
+        true_bins = torch.maximum(torch.tensor(0), true_matches_smoothed)
         pred_bins = torch.maximum(torch.tensor(0), pred_dists)
-        true_dustbin = torch.maximum(torch.tensor(0), 1 - true_matches.sum(dim=0))
+        true_dustbin = torch.maximum(torch.tensor(0), 1 - true_matches_smoothed.sum(dim=0))
         pred_dustbin = torch.maximum(torch.tensor(0), 1 - pred_dists.sum(dim=0))
         
         truth = torch.nn.functional.normalize(torch.vstack((true_bins, true_dustbin)), dim=0, p=1)
-        pred = torch.nn.functional.normalize(torch.vstack((pred_bins, pred_dustbin)) + 1e-9, dim=0, p=1)
+        pred = torch.nn.functional.normalize(torch.vstack((pred_bins, pred_dustbin)), dim=0, p=1)
         
-        gain = torch.log(pred) * truth
+        gain = torch.log(torch.maximum(torch.tensor(1e-12), pred)) * truth
     
         return -gain.sum(), 1
--- src/coffee_nn/coffee_nn/losses/kl.py
+++ src/coffee_nn/coffee_nn/losses/kl.py
@@ -2,22 +2,24 @@ import torch
 from .loss import LossFunction
 
 class KLLoss(LossFunction):
-    def __init__(self, margin, weight):
-        self._margin = margin
-        self._weight = weight
+    def __init__(self):
+        pass
         
     def loss(self, true_dists, true_matches, pred_dists, pred_matches):       
+        true_bins = torch.maximum(torch.tensor(0), true_matches)
+        pred_bins = torch.maximum(torch.tensor(0), pred_dists)
         true_dustbin = torch.maximum(torch.tensor(0), 1 - true_matches.sum(dim=0))
         pred_dustbin = torch.maximum(torch.tensor(0), 1 - pred_dists.sum(dim=0))
 
-        truth = torch.nn.functional.normalize(torch.vstack((true_matches, true_dustbin)), dim=0, p=1)
-        pred = torch.nn.functional.normalize(torch.vstack((pred_dists, pred_dustbin)) + 1e-12, dim=0, p=1)
+        truth = torch.nn.functional.normalize(torch.vstack((true_bins, true_dustbin)), dim=0, p=1)
+        pred = torch.nn.functional.normalize(torch.vstack((pred_bins, pred_dustbin)), dim=0, p=1)
         
         sigma = 2
+        noise_sigmas = 2 # Number of sigmas above which the signal is considered an error
         num_entries = truth.shape[1]
-        noise_likelihood = -2 * torch.ones((1, num_entries))
+        noise_likelihood = -(noise_sigmas)**2/2 * torch.ones((1, num_entries))
         expected = torch.log_softmax(torch.vstack((-true_dists**2/(2*sigma*sigma), noise_likelihood)), dim=0)
 
-        kl_div = torch.sum(torch.exp(expected) * (expected - torch.log(pred)), dim=0)
+        kl_div = torch.sum(torch.exp(expected) * (expected - torch.log(torch.maximum(torch.tensor(1e-12), pred))), dim=0)
         
         return kl_div.sum(), 1
